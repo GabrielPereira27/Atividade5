825a9df54a6017d1721b378691e3dbd458cff04d
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-15 12:23:29 +0100

    Update dois.php, qq.php, and 3 more files...

---
+++ tres.php
@@ -0,0 +1,23 @@
+<?php 
+$txt1 = "Eu sou Joao e";
+$txt2 = "na turma sou o numero";
+$txt3 = "Vou fazer este modulo com";
+$Num1 = "15";
+$Num2 = "20";
+$txt4 = "Gabriel";
+$Num3 = "4";
+$Num4 = "12";
+$txt5 = "Os testes estão no nosso disco de rede em";
+$txt6 = "Em PHP, a declaração de uma variável implica a precedência de um carácter especial: o carater $.";
+$txt7 = "O professor";
+$txt8 = "chegou perto";
+$txt9 = "do aluno e";
+$txt10 = "exclamou: “se não"; 
+$txt11 = "estudares, vais"; 
+$txt12 = "reprovar de ano!”.";
+echo $txt1. " " .$txt2. " " .$Num1. " " .$txt3. " " .$Num2. "Valores.";
+echo "<br>" .$txt4. " " .$Num3. " " .$Num4. "";
+echo "<br>" .$txt5. "\"D:\Gabriel\Atividade5\"";
+echo "<br>" .$txt6;
+echo "<br>" .$txt7. "<br>" .$txt8. "<br>" .$txt9. "<br>" .$txt10. "<br>" .$txt11. "<br>" .$txt12;
+?>
---
+++ um.php
@@ -0,0 +1,11 @@
+<?php // queremos imprimir Guns N' Roses
+echo "Guns N' Roses";
+?>
+
+<?php // queremos imprimir D:\Gabriel\Atividade5
+echo "D:\\Gabriel\Atividade5\\";
+?>
+
+<?php // queremos imprimir "Ser poeta é ser mais alto..."
+echo "\"Ser poeta é ser mais alto...\"";
+?>
